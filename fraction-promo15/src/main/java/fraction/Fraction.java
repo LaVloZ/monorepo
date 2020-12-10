91229315e7e6cff65023e37cef569161fe673752
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2020-12-10 14:08:51 +0100

    Start subtract

---
+++ fraction-promo15/src/main/java/fraction/Fraction.java
@@ -0,0 +1,60 @@
+package fraction;
+
+import java.math.BigInteger;
+import java.util.Objects;
+
+public class Fraction {
+
+    public static final Fraction ZERO = fromInt(0);
+    public final int numerator;
+    public final int denominator;
+
+    private Fraction(int numerator, int denominator) {
+        if(denominator == 0)
+            throw new IllegalArgumentException("Cannot create fraction with zero denominator");
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public static Fraction fromInt(int value) {
+        return of(value, 1);
+    }
+
+    public static Fraction of(int numerator, int denominator) {
+        final int gcd = gcd(numerator, denominator);
+        return new Fraction(numerator / gcd,denominator / gcd);
+    }
+
+    private static int gcd(int numerator, int denominator) {
+        return BigInteger.valueOf(numerator).gcd(BigInteger.valueOf(denominator)).intValue();
+    }
+
+    @Override
+    public String toString() {
+        if(this.denominator == 1)
+            return String.valueOf(this.numerator);
+        return this.numerator + "/" + this.denominator;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        return this.numerator == fraction.numerator && this.denominator == fraction.denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numerator, denominator);
+    }
+
+    public Fraction add(Fraction other) {
+        return of((numerator * other.denominator) + (other.numerator * denominator),
+                denominator * other.denominator);
+    }
+
+    public Fraction subtract(Fraction fraction) {
+        return of(2,5);
+    }
+}
---
+++ fraction-promo15/src/test/java/fraction/FractionSpec.java
@@ -0,0 +1,128 @@
+package fraction;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FractionSpec {
+
+    @Test
+    public void cantDealWithFractionsWithZeroDenominator(){
+        assertThrows(IllegalArgumentException.class, () -> Fraction.of(3,0));
+    }
+
+    @Test
+    public void isEqualToItsSimplifiedForm(){
+        assertEquals(Fraction.fromInt(1),Fraction.of(2,2));
+    }
+
+    @Test
+    public void isEqualToItsUnsimplifiedForm(){
+        assertEquals(Fraction.of(2,2),Fraction.fromInt(1));
+    }
+
+    @Nested
+    class Subtraction {
+        @Test
+        public void test() {
+            assertEquals(Fraction.of(2, 5), Fraction.of(3, 5).subtract(Fraction.of(1, 5)));
+        }
+    }
+
+    @Nested
+    class FormatRules {
+        @Test
+        public void formatIntegerAsInteger() {
+            assertEquals("2", Fraction.fromInt(2).toString());
+        }
+
+        @Test
+        public void formatFractionAsFraction() {
+            assertEquals("2/3", Fraction.of(2, 3).toString());
+        }
+
+        @Test
+        public void formatResultSimplifiedForAdditionTwoFractions() {
+            assertEquals("7/6", Fraction.of(2, 4).add(Fraction.of(4, 6)).toString());
+        }
+
+        @Test
+        public void formatFractionToInteger() {
+            assertEquals("1", Fraction.of(2, 2).toString());
+        }
+
+    }
+
+    @Nested
+    class Simplification {
+        @Test
+        public void doesNothingForSimplifiedFraction() {
+            assertEquals(Fraction.of(2, 3), Fraction.of(2, 3));
+        }
+
+        @Test
+        public void doesNothingForIntegers() {
+            assertEquals(Fraction.fromInt(2), Fraction.fromInt(2));
+        }
+
+        @Test
+        public void simplifyFractionByDividingNumeratorAndDenominatorByGcd() {
+            assertEquals(Fraction.of(2, 3),
+                    Fraction.of(2 * 3 * 5 * 11 * 13 * 17 * 19, 3 * 3 * 5 * 11 * 13 * 17 * 19));
+        }
+    }
+
+    @Nested
+    class AdditionRules {
+        @Test
+        public void zeroPlusZeroEqualsZero() {
+            assertEquals(Fraction.ZERO, Fraction.ZERO.add(Fraction.ZERO));
+        }
+
+        @Test
+        public void zeroIsNeutralInSecondOperandForIntegers() {
+            assertEquals(Fraction.fromInt(1), Fraction.fromInt(1).add(Fraction.ZERO));
+        }
+
+        @Test
+        public void zeroIsNeutralInFirstOperandForIntegers() {
+            assertEquals(Fraction.fromInt(1), Fraction.ZERO.add(Fraction.fromInt(1)));
+        }
+
+        @Test
+        public void canAddTwoIntegers() {
+            assertEquals(Fraction.fromInt(3), Fraction.fromInt(1).add(Fraction.fromInt(2)));
+        }
+
+        @Test
+        public void canAddTwoFractionsWithTheSameDenominatorByAddingTheirNumerators() {
+            assertEquals(Fraction.of(4, 5),
+                    Fraction.of(1, 5).add(Fraction.of(3, 5)));
+        }
+
+        @Test
+        public void canAddPositiveIntegerWithNegativeInteger() {
+            assertEquals(Fraction.fromInt(3), Fraction.fromInt(4).add(Fraction.fromInt(-1)));
+        }
+
+        @Test
+        @DisplayName("The sum of two fractions is the addition of the multiplication of their numerators by the other denominators over the multiplication of their denominators")
+        public void canAddTwoFractionsWithDifferentDominators() {
+            assertEquals(Fraction.of((1 * 3) + (1 * 2), 2 * 3),
+                    Fraction.of(1, 2).add(Fraction.of(1, 3)));
+        }
+
+        @Test
+        public void canAddIntegerWithAFraction() {
+            assertEquals(Fraction.of((2 * 3) + (1 * 1), 1 * 3),
+                    Fraction.fromInt(2).add(Fraction.of(1, 3)));
+        }
+
+    }
+
+
+}
