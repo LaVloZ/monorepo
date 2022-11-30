e862ef4e96b7a66780d0c37af8d0dfcd945cea6a
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-30 02:31:58 +0100

    refacto

--- Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
+++ Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
@@ -31,33 +31,31 @@ public class TennisGame1 implements TennisGame {
                     score = "Deuce";
                     break;
             }
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            int minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
         } else {
-            if (m_score1 >= 4 || m_score2 >= 4) {
-                int minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
-            } else {
-                if (m_score1 == 0) {
-                    score = score + "Love";
-                } else if (m_score1 == 1) {
-                    score = score + "Fifteen";
-                } else if (m_score1 == 2) {
-                    score = score + "Thirty";
-                } else if (m_score1 == 3) {
-                    score = score + "Forty";
-                }
-                score += "-";
-                if (m_score2 == 0) {
-                    score = score + "Love";
-                } else if (m_score2 == 1) {
-                    score = score + "Fifteen";
-                } else if (m_score2 == 2) {
-                    score = score + "Thirty";
-                } else if (m_score2 == 3) {
-                    score = score + "Forty";
-                }
+            if (m_score1 == 0) {
+                score = score + "Love";
+            } else if (m_score1 == 1) {
+                score = score + "Fifteen";
+            } else if (m_score1 == 2) {
+                score = score + "Thirty";
+            } else if (m_score1 == 3) {
+                score = score + "Forty";
+            }
+            score += "-";
+            if (m_score2 == 0) {
+                score = score + "Love";
+            } else if (m_score2 == 1) {
+                score = score + "Fifteen";
+            } else if (m_score2 == 2) {
+                score = score + "Thirty";
+            } else if (m_score2 == 3) {
+                score = score + "Forty";
             }
         }
         return score;
