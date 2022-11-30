ccc0ddd36cb028f5bdaf3d4c1fa389c590f8d1fe
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-30 02:54:06 +0100

    refacto

--- Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
+++ Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
@@ -21,16 +21,16 @@ public class TennisGame1 implements TennisGame {
                 String all = "-All";
                 score = "Love" + all;
             } else {
-                if (m_score1 != 1) {
+                if (m_score1 == 1) {
+                    String all = "-All";
+                    score = "Fifteen" + all;
+                } else {
                     if (m_score1 == 2) {
                         String all = "-All";
                         score = "Thirty" + all;
                     } else {
                         score = "Deuce";
                     }
-                } else {
-                    String all = "-All";
-                    score = "Fifteen" + all;
                 }
             }
         } else if (m_score1 >= 4 || m_score2 >= 4) {
