5acd33ead1b743e5a8604a2171343d4b7f592bc6
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-30 02:39:41 +0100

    refacto

--- Tennis-Refactoring-Kata/java/src/main/java/TennisGame3.java
+++ Tennis-Refactoring-Kata/java/src/main/java/TennisGame3.java
@@ -3,12 +3,12 @@ public class TennisGame3 implements TennisGame {
     
     private int p2;
     private int p1;
-    private final String p1N;
-    private final String p2N;
+    private final String player1;
+    private final String player2;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
     public String getScore() {
@@ -20,7 +20,7 @@ public class TennisGame3 implements TennisGame {
         } else {
             if (p1 == p2)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
+            s = p1 > p2 ? player1 : player2;
             return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
