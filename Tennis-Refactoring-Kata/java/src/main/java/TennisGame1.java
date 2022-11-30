72eafa0d3335bc3eb140a312424fd7b40bb3c137
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-30 02:12:25 +0100

    refacto

--- Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
+++ Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
@@ -4,7 +4,7 @@ public class TennisGame1 implements TennisGame {
     private int m_score1 = 0;
     private int m_score2 = 0;
 
-    public TennisGame1(String player1Name, String player2Name) {
+    public TennisGame1() {
     }
 
     public void wonPoint(String playerName) {
