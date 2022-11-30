377bf681846cd460eeb33711fc8414c3db0623fe
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-30 04:26:12 +0100

    refacto

--- Tennis-Refactoring-Kata/java/src/main/java/TennisGame2.java
+++ Tennis-Refactoring-Kata/java/src/main/java/TennisGame2.java
@@ -19,68 +19,66 @@ public class TennisGame2 implements TennisGame {
             if (score1 == 2)
                 return "Thirty" + "-All";
         }
-        else {
-            if (score1 == score2 && score1 >= 3)
-                return "Deuce";
+        if (score1 == score2 && score1 >= 3)
+            return "Deuce";
 
-            if (score1 > 0 && score2 == 0) {
-                if (score1 == 1)
-                    player1 = "Fifteen";
-                if (score1 == 2)
-                    player1 = "Thirty";
-                if (score1 == 3)
-                    player1 = "Forty";
+        if (score1 > 0 && score2 == 0) {
+            if (score1 == 1)
+                player1 = "Fifteen";
+            if (score1 == 2)
+                player1 = "Thirty";
+            if (score1 == 3)
+                player1 = "Forty";
 
-                player2 = "Love";
-                score = player1 + "-" + player2;
-            }
-            if (score2 > 0 && score1 == 0) {
-                if (score2 == 1)
-                    player2 = "Fifteen";
-                if (score2 == 2)
-                    player2 = "Thirty";
-                if (score2 == 3)
-                    player2 = "Forty";
+            player2 = "Love";
+            score = player1 + "-" + player2;
+        }
+        if (score2 > 0 && score1 == 0) {
+            if (score2 == 1)
+                player2 = "Fifteen";
+            if (score2 == 2)
+                player2 = "Thirty";
+            if (score2 == 3)
+                player2 = "Forty";
 
-                player1 = "Love";
-                score = "Love" + "-" + player2;
-            }
+            player1 = "Love";
+            score = "Love" + "-" + player2;
+        }
 
-            if (score1 > score2 && score1 < 4) {
-                if (score1 == 2)
-                    player1 = "Thirty";
-                if (score1 == 3)
-                    player1 = "Forty";
-                if (score2 == 1)
-                    player2 = "Fifteen";
-                if (score2 == 2)
-                    player2 = "Thirty";
-                return player1 + "-" + player2;
-            }
+        if (score1 > score2 && score1 < 4) {
+            if (score1 == 2)
+                player1 = "Thirty";
+            if (score1 == 3)
+                player1 = "Forty";
+            if (score2 == 1)
+                player2 = "Fifteen";
+            if (score2 == 2)
+                player2 = "Thirty";
+            return player1 + "-" + player2;
+        }
 
-            if (score2 > score1 && score2 < 4) {
-                if (score2 == 2)
-                    player2 = "Thirty";
-                if (score2 == 3)
-                    player2 = "Forty";
-                if (score1 == 1)
-                    return "Fifteen" + "-" + player2;
-                if (score1 == 2)
-                    return "Thirty" + "-" + player2;
-                return player1 + "-" + player2;
-            }
-            if (score1 - score2 >= 2) {
-                return "Win for player1";
-            }
-            if (score2 - score1 >= 2) {
-                return "Win for player2";
-            }
-            if (score1 > score2) {
-                return "Advantage player1";
-            }
-            if (score2 > score1) {
-                return "Advantage player2";
-            }
+        if (score2 > score1 && score2 < 4) {
+            if (score2 == 2)
+                player2 = "Thirty";
+            if (score2 == 3)
+                player2 = "Forty";
+            if (score1 == 1)
+                return "Fifteen" + "-" + player2;
+            if (score1 == 2)
+                return "Thirty" + "-" + player2;
+            return player1 + "-" + player2;
+        }
+        if (score1 - score2 >= 2) {
+            return "Win for player1";
+        }
+        if (score2 - score1 >= 2) {
+            return "Win for player2";
+        }
+        if (score1 > score2) {
+            return "Advantage player1";
+        }
+        if (score2 > score1) {
+            return "Advantage player2";
         }
 
         return score;
