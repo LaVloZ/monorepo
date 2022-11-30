16b0a6d0d7d894e0beef75e5ee265112dc11a27e
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-30 03:25:41 +0100

    refacto

--- Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
+++ Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
@@ -1,34 +1,34 @@
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int score1 = 0;
+    private int score2 = 0;
 
     public TennisGame1() {
     }
 
     public void wonPoint(String playerName) {
         if ("player1".equals(playerName))
-            m_score1 += 1;
+            score1 += 1;
         else
-            m_score2 += 1;
+            score2 += 1;
     }
 
     public String getScore() {
-        if (m_score1 == m_score2) {
-            if (m_score1 <= 2) {
-                return getScore(m_score1) + "-All";
+        if (score1 == score2) {
+            if (score1 <= 2) {
+                return getScore(score1) + "-All";
             }
             return "Deuce";
         }
-        if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
+        if (score1 >= 4 || score2 >= 4) {
+            int minusResult = score1 - score2;
             if (minusResult == 1) return "Advantage " + "player1";
             if (minusResult == -1) return "Advantage " + "player2";
             if (minusResult >= 2) return "Win for " + "player1";
             return "Win for " + "player2";
         }
-        return getScore(m_score1) + "-" + getScore(m_score2);
+        return getScore(score1) + "-" + getScore(score2);
     }
 
     private String getScore(int score) {
