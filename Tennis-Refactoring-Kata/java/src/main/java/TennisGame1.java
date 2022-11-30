ce1d83e277761ab18f17a7adc2af061e36fd16a7
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-30 03:17:52 +0100

    refacto

--- Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
+++ Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
@@ -17,10 +17,10 @@ public class TennisGame1 implements TennisGame {
     public String getScore() {
         String score;
         if (m_score1 == m_score2) {
-            if (m_score1 > 2) {
-                return "Deuce";
+            if (m_score1 <= 2) {
+                return getScore(m_score1) + "-All";
             }
-            return getScore(m_score1) + "-All";
+            return "Deuce";
         } else if (m_score1 >= 4 || m_score2 >= 4) {
             int minusResult = m_score1 - m_score2;
             if (minusResult == 1) score = "Advantage player1";
