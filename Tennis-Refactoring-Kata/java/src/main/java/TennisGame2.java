fb8749962103d4d169465fedd6e41ca6b869296f
Abdessamed MANSOURI <abdessamed.mansouri@stonal.com>
2022-11-30 03:59:36 +0100

    refacto

--- Tennis-Refactoring-Kata/java/src/main/java/TennisGame2.java
+++ Tennis-Refactoring-Kata/java/src/main/java/TennisGame2.java
@@ -1,106 +1,96 @@
 
-public class TennisGame2 implements TennisGame
-{
+public class TennisGame2 implements TennisGame {
     public int score1 = 0;
     public int score = 0;
-    
+
     public String player1 = "";
     public String player2 = "";
 
     public TennisGame2() {
     }
 
-    public String getScore(){
+    public String getScore() {
         String score = "";
-        if (score1 == this.score && score1 < 4)
-        {
-            if (score1 ==0)
+        if (score1 == this.score && score1 < 4) {
+            if (score1 == 0)
                 return "Love" + "-All";
-            if (score1 ==1)
+            if (score1 == 1)
                 return "Fifteen" + "-All";
-            if (score1 ==2)
+            if (score1 == 2)
                 score = "Thirty";
             score += "-All";
         }
-        if (score1 == this.score && score1 >=3)
+        if (score1 == this.score && score1 >= 3)
             score = "Deuce";
-        
-        if (score1 > 0 && this.score ==0)
-        {
-            if (score1 ==1)
+
+        if (score1 > 0 && this.score == 0) {
+            if (score1 == 1)
                 player1 = "Fifteen";
-            if (score1 ==2)
+            if (score1 == 2)
                 player1 = "Thirty";
-            if (score1 ==3)
+            if (score1 == 3)
                 player1 = "Forty";
-            
+
             player2 = "Love";
             score = player1 + "-" + player2;
         }
-        if (this.score > 0 && score1 ==0)
-        {
-            if (this.score ==1)
+        if (this.score > 0 && score1 == 0) {
+            if (this.score == 1)
                 player2 = "Fifteen";
-            if (this.score ==2)
+            if (this.score == 2)
                 player2 = "Thirty";
-            if (this.score ==3)
+            if (this.score == 3)
                 player2 = "Forty";
-            
+
             player1 = "Love";
             score = "Love" + "-" + player2;
         }
-        
-        if (score1 > this.score && score1 < 4)
-        {
-            if (score1 ==2)
-                player1 ="Thirty";
-            if (score1 ==3)
-                player1 ="Forty";
-            if (this.score ==1)
-                player2 ="Fifteen";
-            if (this.score ==2)
-                player2 ="Thirty";
+
+        if (score1 > this.score && score1 < 4) {
+            if (score1 == 2)
+                player1 = "Thirty";
+            if (score1 == 3)
+                player1 = "Forty";
+            if (this.score == 1)
+                player2 = "Fifteen";
+            if (this.score == 2)
+                player2 = "Thirty";
             score = player1 + "-" + player2;
         }
-        if (this.score > score1 && this.score < 4)
-        {
-            if (this.score ==2)
-                player2 ="Thirty";
-            if (this.score ==3)
-                player2 ="Forty";
-            if (score1 ==1)
-                player1 ="Fifteen";
-            if (score1 ==2)
-                player1 ="Thirty";
+        if (this.score > score1 && this.score < 4) {
+            if (this.score == 2)
+                player2 = "Thirty";
+            if (this.score == 3)
+                player2 = "Forty";
+            if (score1 == 1)
+                player1 = "Fifteen";
+            if (score1 == 2)
+                player1 = "Thirty";
             score = player1 + "-" + player2;
         }
 
-        if (score1 > this.score && this.score >= 3)
-        {
+        if (score1 > this.score && this.score >= 3) {
             score = "Advantage player1";
         }
-        
-        if (this.score > score1 && score1 >= 3)
-        {
+
+        if (this.score > score1 && score1 >= 3) {
             score = "Advantage player2";
         }
-        
-        if (score1 >=4 && this.score >=0 && (score1 - this.score)>=2)
-        {
+
+        if (score1 >= 4 && this.score >= 0 && (score1 - this.score) >= 2) {
             return "Win for player1";
         }
-        if (this.score >=4 && score1 >=0 && (this.score - score1)>=2)
-        {
+        if (this.score >= 4 && score1 >= 0 && (this.score - score1) >= 2) {
             return "Win for player2";
         }
         return score;
     }
 
-    public void P1Score(){
+    public void P1Score() {
         score1++;
     }
-    
-    public void P2Score(){
+
+    public void P2Score() {
         score++;
     }
 
