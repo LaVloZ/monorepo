7b300add657730c027ce5388b1b6a18e60937897
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-02-22 20:40:44 +0100

    refacto

--- SingSongKata/src/main/java/Song.java
+++ SingSongKata/src/main/java/Song.java
@@ -1,3 +1,4 @@
+import toto.Animal;
 import toto.Animals;
 
 class Song {
@@ -8,52 +9,19 @@ class Song {
 
     public static void main(String[] args) {
 
-        String song = "There was an old lady who swallowed a fly.\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a spider;\n" +
-                "That wriggled and wiggled and tickled inside her.\n" +
-                "She swallowed the spider to catch the fly;\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a bird;\n" +
-                "How absurd to swallow a bird.\n" +
-                "She swallowed the bird to catch the spider,\n" +
-                "She swallowed the spider to catch the fly;\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a cat;\n" +
-                "Fancy that to swallow a cat!\n" +
-                "She swallowed the cat to catch the bird,\n" +
-                "She swallowed the bird to catch the spider,\n" +
-                "She swallowed the spider to catch the fly;\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a dog;\n" +
-                "What a hog, to swallow a dog!\n" +
-                "She swallowed the dog to catch the cat,\n" +
-                "She swallowed the cat to catch the bird,\n" +
-                "She swallowed the bird to catch the spider,\n" +
-                "She swallowed the spider to catch the fly;\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a cow;\n" +
-                "I don't know how she swallowed a cow!\n" +
-                "She swallowed the cow to catch the dog,\n" +
-                "She swallowed the dog to catch the cat,\n" +
-                "She swallowed the cat to catch the bird,\n" +
-                "She swallowed the bird to catch the spider,\n" +
-                "She swallowed the spider to catch the fly;\n" +
-                "I don't know why she swallowed a fly - perhaps she'll die!\n" +
-                "\n" +
-                "There was an old lady who swallowed a horse...\n" +
-                "...She's dead, of course!";
-
-        System.out.println(song);
+        System.out.println(Song.sing(
+                new Animals(
+                        new Animal("fly", ""),
+                        new Animal("spider", "That wriggled and wiggled and tickled inside her."),
+                        new Animal("bird", "How absurd to swallow a {name}."),
+                        new Animal("cat", "Fancy that to swallow a {name}!"),
+                        new Animal("dog", "What a hog, to swallow a {name}!"),
+                        new Animal("cow", "I don't know how she swallowed a {name}!"),
+                        new Animal("horse", ""))));
     }
 
     public static String sing(Animals animals) {
-        String song = THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.fly() + ".\n" +
+        return THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.fly() + ".\n" +
                 I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.fly() + " - perhaps she'll die!\n" +
                 "\n" +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.spider() + ";\n" +
@@ -93,7 +61,6 @@ class Song {
                 "\n" +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.horse() + "...\n" +
                 "...She's dead, of course!";
-        return song;
     }
 
 }
