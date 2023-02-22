43b48bfcef7906657bef88faeba05d8db0911599
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-02-22 20:25:17 +0100

    refacto

--- SingSongKata/src/main/java/toto/Animal.java
+++ SingSongKata/src/main/java/toto/Animal.java
@@ -2,9 +2,16 @@ package toto;
 
 public class Animal {
     private final String name;
+    private String rhyme;
+
+    public Animal(String cat, String rhyme) {
+        this.name = cat;
+        this.rhyme = rhyme;
+    }
 
     public Animal(String cat) {
         this.name = cat;
+        this.rhyme = "";
     }
 
     public String rhyme() {
