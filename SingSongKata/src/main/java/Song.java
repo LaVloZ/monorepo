924af8fd1dababd238eb3e9e8ae7b30f3275f820
kiaa <diallorokiatou1@gmail.com>
2023-02-22 19:43:02 +0100

    add parameters

--- SingSongKata/src/main/java/Song.java
+++ SingSongKata/src/main/java/Song.java
@@ -61,17 +61,17 @@ class Song {
                 "She swallowed the " + spider + " to catch the " + fly + ";\n" +
                 "I don't know why she swallowed a " + fly + " - perhaps she'll die!\n" +
                 "\n" +
-                "There was an old lady who swallowed a cat;\n" +
-                "Fancy that to swallow a cat!\n" +
-                "She swallowed the cat to catch the " + bird + ",\n" +
+                "There was an old lady who swallowed a " + cat + ";\n" +
+                "Fancy that to swallow a " + cat + "!\n" +
+                "She swallowed the "+ cat + " to catch the " + bird + ",\n" +
                 "She swallowed the " + bird + " to catch the " + spider + ",\n" +
                 "She swallowed the " + spider + " to catch the " + fly + ";\n" +
                 "I don't know why she swallowed a " + fly + " - perhaps she'll die!\n" +
                 "\n" +
                 "There was an old lady who swallowed a " + dog + ";\n" +
                 "What a hog, to swallow a " + dog + "!\n" +
-                "She swallowed the " + dog + " to catch the cat,\n" +
-                "She swallowed the cat to catch the " + bird + ",\n" +
+                "She swallowed the " + dog + " to catch the "+ cat +",\n" +
+                "She swallowed the " + cat + " to catch the " + bird + ",\n" +
                 "She swallowed the " + bird + " to catch the " + spider + ",\n" +
                 "She swallowed the " + spider + " to catch the " + fly + ";\n" +
                 "I don't know why she swallowed a " + fly + " - perhaps she'll die!\n" +
@@ -79,8 +79,8 @@ class Song {
                 "There was an old lady who swallowed a " + cow + ";\n" +
                 "I don't know how she swallowed a " + cow + "!\n" +
                 "She swallowed the " + cow + " to catch the " + dog + ",\n" +
-                "She swallowed the " + dog + " to catch the cat,\n" +
-                "She swallowed the cat to catch the " + bird + ",\n" +
+                "She swallowed the " + dog + " to catch the " + cat +",\n" +
+                "She swallowed the " + cat +" to catch the " + bird + ",\n" +
                 "She swallowed the " + bird + " to catch the " + spider + ",\n" +
                 "She swallowed the " + spider + " to catch the " + fly + ";\n" +
                 "I don't know why she swallowed a " + fly + " - perhaps she'll die!\n" +
