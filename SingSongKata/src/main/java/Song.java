d99f39b6dd8a32153bcdb85b36b3cdb22ff42d5c
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-02-22 19:57:32 +0100

    refacto

--- SingSongKata/src/main/java/Song.java
+++ SingSongKata/src/main/java/Song.java
@@ -3,6 +3,8 @@ import toto.Animals;
 class Song {
 
     public static final String THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A = "There was an old lady who swallowed a ";
+    public static final String SHE_SWALLOWED_THE = "She swallowed the ";
+    public static final String I_DON_T_KNOW_WHY_SHE_SWALLOWED_A = "I don't know why she swallowed a ";
 
     public static void main(String[] args) {
 
@@ -52,42 +54,42 @@ class Song {
 
     public static String sing(Animals animals) {
         String song = THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.fly() + ".\n" +
-                "I don't know why she swallowed a " + animals.fly() + " - perhaps she'll die!\n" +
+                I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.fly() + " - perhaps she'll die!\n" +
                 "\n" +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.spider() + ";\n" +
                 "That wriggled and wiggled and tickled inside her.\n" +
-                "She swallowed the " + animals.spider() + " to catch the " + animals.fly() + ";\n" +
-                "I don't know why she swallowed a " + animals.fly() + " - perhaps she'll die!\n" +
+                SHE_SWALLOWED_THE + animals.spider() + " to catch the " + animals.fly() + ";\n" +
+                I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.fly() + " - perhaps she'll die!\n" +
                 "\n" +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.bird() + ";\n" +
                 "How absurd to swallow a " + animals.bird() + ".\n" +
-                "She swallowed the " + animals.bird() + " to catch the " + animals.spider() + ",\n" +
-                "She swallowed the " + animals.spider() + " to catch the " + animals.fly() + ";\n" +
-                "I don't know why she swallowed a " + animals.fly() + " - perhaps she'll die!\n" +
+                SHE_SWALLOWED_THE + animals.bird() + " to catch the " + animals.spider() + ",\n" +
+                SHE_SWALLOWED_THE + animals.spider() + " to catch the " + animals.fly() + ";\n" +
+                I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.fly() + " - perhaps she'll die!\n" +
                 "\n" +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.cat() + ";\n" +
                 "Fancy that to swallow a " + animals.cat() + "!\n" +
-                "She swallowed the "+ animals.cat() + " to catch the " + animals.bird() + ",\n" +
-                "She swallowed the " + animals.bird() + " to catch the " + animals.spider() + ",\n" +
-                "She swallowed the " + animals.spider() + " to catch the " + animals.fly() + ";\n" +
-                "I don't know why she swallowed a " + animals.fly() + " - perhaps she'll die!\n" +
+                SHE_SWALLOWED_THE + animals.cat() + " to catch the " + animals.bird() + ",\n" +
+                SHE_SWALLOWED_THE + animals.bird() + " to catch the " + animals.spider() + ",\n" +
+                SHE_SWALLOWED_THE + animals.spider() + " to catch the " + animals.fly() + ";\n" +
+                I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.fly() + " - perhaps she'll die!\n" +
                 "\n" +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.dog() + ";\n" +
                 "What a hog, to swallow a " + animals.dog() + "!\n" +
-                "She swallowed the " + animals.dog() + " to catch the "+ animals.cat() +",\n" +
-                "She swallowed the " + animals.cat() + " to catch the " + animals.bird() + ",\n" +
-                "She swallowed the " + animals.bird() + " to catch the " + animals.spider() + ",\n" +
-                "She swallowed the " + animals.spider() + " to catch the " + animals.fly() + ";\n" +
-                "I don't know why she swallowed a " + animals.fly() + " - perhaps she'll die!\n" +
+                SHE_SWALLOWED_THE + animals.dog() + " to catch the "+ animals.cat() +",\n" +
+                SHE_SWALLOWED_THE + animals.cat() + " to catch the " + animals.bird() + ",\n" +
+                SHE_SWALLOWED_THE + animals.bird() + " to catch the " + animals.spider() + ",\n" +
+                SHE_SWALLOWED_THE + animals.spider() + " to catch the " + animals.fly() + ";\n" +
+                I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.fly() + " - perhaps she'll die!\n" +
                 "\n" +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.cow() + ";\n" +
                 "I don't know how she swallowed a " + animals.cow() + "!\n" +
-                "She swallowed the " + animals.cow() + " to catch the " + animals.dog() + ",\n" +
-                "She swallowed the " + animals.dog() + " to catch the " + animals.cat() +",\n" +
-                "She swallowed the " + animals.cat() +" to catch the " + animals.bird() + ",\n" +
-                "She swallowed the " + animals.bird() + " to catch the " + animals.spider() + ",\n" +
-                "She swallowed the " + animals.spider() + " to catch the " + animals.fly() + ";\n" +
-                "I don't know why she swallowed a " + animals.fly() + " - perhaps she'll die!\n" +
+                SHE_SWALLOWED_THE + animals.cow() + " to catch the " + animals.dog() + ",\n" +
+                SHE_SWALLOWED_THE + animals.dog() + " to catch the " + animals.cat() +",\n" +
+                SHE_SWALLOWED_THE + animals.cat() +" to catch the " + animals.bird() + ",\n" +
+                SHE_SWALLOWED_THE + animals.bird() + " to catch the " + animals.spider() + ",\n" +
+                SHE_SWALLOWED_THE + animals.spider() + " to catch the " + animals.fly() + ";\n" +
+                I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.fly() + " - perhaps she'll die!\n" +
                 "\n" +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.horse() + "...\n" +
                 "...She's dead, of course!";
