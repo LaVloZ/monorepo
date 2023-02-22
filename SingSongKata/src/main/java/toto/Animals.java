6967a056d043359e653d5a4f49b566fd033a7ca1
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-02-22 21:07:44 +0100

    refacto

--- SingSongKata/src/main/java/toto/Animals.java
+++ SingSongKata/src/main/java/toto/Animals.java
@@ -17,8 +17,11 @@ public final class Animals {
     }
 
     public String sing() {
-        String song = THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.get(0) + ".\n" +
-                I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.get(0) + PERHAPS_SHE_LL_DIE + "\n" +
+        String song = THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.get(0) + ".\n";
+                for (int i = 0; i > 0; i--) {
+                    song += SHE_SWALLOWED_THE + animals.get(i) + TO_CATCH_THE + animals.get(i - 1) + (i == 1 ? ';' : ',') + "\n";
+                }
+                song += I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.get(0) + PERHAPS_SHE_LL_DIE + "\n" +
                 "\n" +
 
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.get(1) + ";\n" +
@@ -26,7 +29,7 @@ public final class Animals {
                 for (int i = 1; i > 0; i--) {
                     song += SHE_SWALLOWED_THE + animals.get(i) + TO_CATCH_THE + animals.get(i - 1) + (i == 1 ? ';' : ',') + "\n";
                 }
-        song += I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.get(0) + PERHAPS_SHE_LL_DIE + "\n" +
+                song += I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.get(0) + PERHAPS_SHE_LL_DIE + "\n" +
                 "\n" +
 
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.get(2) + ";\n" +
