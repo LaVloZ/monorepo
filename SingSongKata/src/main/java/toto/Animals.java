a16b12cd874a452c438bb28476cc7db3f60c9c85
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-02-22 21:06:44 +0100

    refacto

--- SingSongKata/src/main/java/toto/Animals.java
+++ SingSongKata/src/main/java/toto/Animals.java
@@ -22,9 +22,11 @@ public final class Animals {
                 "\n" +
 
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.get(1) + ";\n" +
-                animals.get(1).rhyme() + "\n" +
-                SHE_SWALLOWED_THE + animals.get(1) + TO_CATCH_THE + animals.get(0) + ";\n" +
-                I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.get(0) + PERHAPS_SHE_LL_DIE + "\n" +
+                animals.get(1).rhyme() + "\n";
+                for (int i = 1; i > 0; i--) {
+                    song += SHE_SWALLOWED_THE + animals.get(i) + TO_CATCH_THE + animals.get(i - 1) + (i == 1 ? ';' : ',') + "\n";
+                }
+        song += I_DON_T_KNOW_WHY_SHE_SWALLOWED_A + animals.get(0) + PERHAPS_SHE_LL_DIE + "\n" +
                 "\n" +
 
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + animals.get(2) + ";\n" +
